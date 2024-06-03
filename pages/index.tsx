ec7e3bb611e74c443e824b4def328b858bb912e0
Kevv55 <kefumo@mun.ca>
2024-06-03 12:47:15 -0230

    added authenticator

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ export default function App() {
   return (
     <main>
       <h1>My todos</h1>
+      <p>Trial...</p>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
